abf1da0e57387d4b14736b247fa41ea5301f2e87
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-11-28 23:16:25 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,54 @@
-const CACHE_NAME = “adjustment-v1”;
+const CACHE_NAME = "adjustment-v1";
 
 const urlsToCache = [
-“./”,
-“./index.html”,
-“./manifest.json”,
-“./assets/footer-lite.html”,
-“./assets/footer-lite.css”,
+"./",
+"./index.html",
+"./manifest.json",
+"./assets/footer-lite.html",
+"./assets/footer-lite.css",
 
 /* ==== 追加：lesson01関連 ==== */
-“./coach/lesson01/index.html”,
-“./coach/lesson01/images/page02.png”,
-“./coach/lesson01/images/page03-1.png”,
-“./coach/lesson01/images/page03-2.png”,
-“./coach/lesson01/images/page03-3.png”,
-“./coach/lesson01/images/page03-4.png”,
-“./coach/lesson01/images/page07.png”,
-“./coach/lesson01/images/page10.png”,
-“./coach/lesson01/images/page11.png”,
-“./coach/lesson01/images/page12.png”,
-“./coach/lesson01/images/page13.png”,
-“./coach/lesson01/images/page17.png”
+"./coach/lesson01/index.html",
+"./coach/lesson01/images/page02.png",
+"./coach/lesson01/images/page03-1.png",
+"./coach/lesson01/images/page03-2.png",
+"./coach/lesson01/images/page03-3.png",
+"./coach/lesson01/images/page03-4.png",
+"./coach/lesson01/images/page07.png",
+"./coach/lesson01/images/page10.png",
+"./coach/lesson01/images/page11.png",
+"./coach/lesson01/images/page12.png",
+"./coach/lesson01/images/page13.png",
+"./coach/lesson01/images/page17.png"
 ];
 
-self.addEventListener(“install”, event => {
+/* ===== INSTALL ===== */
+self.addEventListener('install', (event) => {
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
 );
 });
 
-self.addEventListener(“fetch”, event => {
-event.respondWith(
-caches.match(event.request).then(response => response || fetch(event.request))
+/* ===== ACTIVATE ===== */
+self.addEventListener('activate', (event) => {
+event.waitUntil(
+caches.keys().then((keys) =>
+Promise.all(
+keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+)
+)
 );
+});
+
+/* ===== FETCH ===== */
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+
+      if (response) return response;
+
+      return fetch(event.request).catch(() => caches.match("./index.html"));
+
+    })
+  );
 });
